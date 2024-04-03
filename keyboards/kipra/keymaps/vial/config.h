1db3a50b290f19b998f76a36da8ea0eaed5372be
Peter Lyons <pete@peterlyons.com>
2024-04-02 21:17:47 -0400

    kipra vial support mostly working (layout slightly off)

---
+++ keyboards/kipra/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC3, 0xCE, 0xA6, 0xCB, 0x00, 0x6A, 0x58, 0x9A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 7, 0 }
+

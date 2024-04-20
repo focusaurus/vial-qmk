3795410f8b686b4b599a54e866ed1a6b4388e5f5
Peter Lyons <pete@peterlyons.com>
2024-04-20 13:15:08 -0400

    failed experiments trying to get the splits talking correctly

--- keyboards/kipra/halconf.h
+++ keyboards/kipra/halconf.h
@@ -1,5 +1,8 @@
-// Copyright 2022 jack (@waffle87)
+// Copyright 2023 Danny Nguyen (danny@keeb.io)
 // SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
+
 #define HAL_USE_I2C TRUE
+
 #include_next <halconf.h>
